e4c2980f0be8050d99b40d8c9d1878354ede7f0a
Zion Odebamike <104022759+OluwaZeezu@users.noreply.github.com>
2022-07-05 16:49:35 +0100

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -5,26 +5,26 @@
  */
 int main(void)
 {
-	int i, n;
-	long f1, f2, next;
+int i, n;
+long f1, f2, next;
 
-	f1 = 1;
-	f2 = 2;
-	n = 50;
-
-	for (i = 0; i < n; i++)
-	{
-		if (i == 49)
-		{
-			printf("%lu", f1);
-		} else
-		{
-			printf("%lu, ", f1);
-			next = f1 + f2;
-			f1 = f2;
-			f2 = next;
-		}
-	}
-	printf("\n");
-	return (0);
+f1 = 1;
+f2 = 2;
+n = 50;
+for (i = 0; i < n; i++)
+{
+if (i == 49)
+{
+printf("%lu", f1);
+}
+else
+{
+printf("%lu, ", f1);
+next = f1 + f2;
+f1 = f2;
+f2 = next;
+}
+}
+printf("\n");
+return (0);
 }
